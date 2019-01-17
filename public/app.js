946a77f658050a503529281edc8c24785d52f5c0
Paul Young <paul@MacMini.local>
2019-01-17 18:48:42 +1100

    added DB functionality

--- public/app.js
+++ public/app.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", event => {
 	const app = firebase.app()
 	console.log(app)
-})
 
+	// told to add via firestore to get timestamps working
+	firebase.firestore().settings({ timestampsInSnapshots: true});
+
+	const db = firebase.firestore()
+	const myPost = db.collection('posts').doc('firstpost')
+	myPost.get()
+	.then( doc=>{
+		const data = doc.data()
+		document.write(data.title+ '<br/>')  
+		document.write(data.views)
+		document.write(data.createdAt.toDate())
+	})
+})
+ 
 function googleLogin() {
 	const provider = new firebase.auth.GoogleAuthProvider()
 	firebase.auth().signInWithPopup(provider)
