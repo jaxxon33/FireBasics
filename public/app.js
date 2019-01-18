c26d5e52f9d7e9446791d5984b07ca9d0fe2c19c
Paul Young <paul@MacMini.local>
2019-01-19 09:33:37 +1100

    got rid of document.write and tageted id instead

--- public/app.js
+++ public/app.js
@@ -5,35 +5,30 @@ document.addEventListener("DOMContentLoaded", event => {
 	// told to add via firestore to get timestamps working
 	firebase.firestore().settings({ timestampsInSnapshots: true });
 
+	// connect to db
 	const db = firebase.firestore()
 	const myPost = db.collection('posts').doc('firstpost')
 
 	// retrieve item
 	myPost.get()
-	.then( doc=>{
-		const data = doc.data()
-		document.write(data.title+ '<br/>')  
-		document.write(data.views)
-		document.write(data.createdAt.toDate() + '<br />')
-	})
+		.then(doc => {
+			const data = doc.data()
+			first.innerHTML = `${data.title} ${data.views} ${data.createdAt.toDate()}`
+		})
 
 	// get changes as they happen
 	myPost.onSnapshot(doc => {
 		const data = doc.data()
-		document.write(data.title + '<br/>')
-		document.write(data.views)
-		document.write(data.createdAt.toDate() + '<br />')
+		second.innerHTML = `${data.title} ${data.views} ${data.createdAt.toDate()}`
 	})
 })
 
+// auth
 function googleLogin() {
 	const provider = new firebase.auth.GoogleAuthProvider()
 	firebase.auth().signInWithPopup(provider)
 		.then(result => {
 			const user = result.user
-			document.write(`Hello ${user.displayName}`)
-			console.log(user)
+			heading.innerHTML = `Hello ${user.displayName}`
 		})
-		.catch(console.log)
-
 }
