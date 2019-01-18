e3c5dd56dce65285498db798b9d380b2c80b5c06
Paul Young <paul@MacMini.local>
2019-01-18 11:04:11 +1100

    added onSnaphot method

--- public/app.js
+++ public/app.js
@@ -3,19 +3,29 @@ document.addEventListener("DOMContentLoaded", event => {
 	console.log(app)
 
 	// told to add via firestore to get timestamps working
-	firebase.firestore().settings({ timestampsInSnapshots: true});
+	firebase.firestore().settings({ timestampsInSnapshots: true });
 
 	const db = firebase.firestore()
 	const myPost = db.collection('posts').doc('firstpost')
+
+	// retrieve item
 	myPost.get()
 	.then( doc=>{
 		const data = doc.data()
 		document.write(data.title+ '<br/>')  
 		document.write(data.views)
-		document.write(data.createdAt.toDate())
+		document.write(data.createdAt.toDate() + '<br />')
+	})
+
+	// get changes as they happen
+	myPost.onSnapshot(doc => {
+		const data = doc.data()
+		document.write(data.title + '<br/>')
+		document.write(data.views)
+		document.write(data.createdAt.toDate() + '<br />')
 	})
 })
- 
+
 function googleLogin() {
 	const provider = new firebase.auth.GoogleAuthProvider()
 	firebase.auth().signInWithPopup(provider)
